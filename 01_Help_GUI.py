60de2bb59801b6342701316dd66ac12574ba3f89
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-02-09 10:49:42 +1300

    01_Help_GUI
    Added more code good so far

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -1,15 +1,24 @@
 from tkinter import *
+from functools import partial # To prevent unwanted windows
+
 import random
 
 
-class Convertor:
-    def __init__(self, parent):
-        print("hello world")
+class Converter:
+    def __init__(self):
+
+        # Formatting variables..
+        background_color = "light blue"
+
+        # Converter Main Screen GUI..
+        self.converter_frame = Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
+
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Convertor")
-    something = Convertor(root)
+    something = Converter(root)
     root.mainloop()
