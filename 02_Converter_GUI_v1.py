b7175a065377f23fef63121e011bba35a062f958
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-02-12 12:56:43 +1300

    02_Converter_GUI
    more code. Should work

--- 02_Converter_GUI_v1.py
+++ 02_Converter_GUI_v1.py
@@ -20,11 +20,22 @@ class Converter:
                                         font="Arial 16 bold",
                                         bg=background_color,
                                         padx=10, pady=10)
-        self.temp_heading_label.grid(row=1)
+        self.temp_heading_label.grid(row=0)
 
         # User instructions (row 1)
+        self.temperature_instructions_label = Label(self.converter_frame,
+                                                    text="Type in the amount to be"
+                                                    "converted and then push"
+                                                    "one of the buttons below...",
+                                                    font="Arial 10 italic", wrap=250,
+                                                    justify=LEFT, bg=background_color,
+                                                    padx=10, pady=10)
+        self.temperature_instructions_label.grid(row=1)
 
         # Temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font="Arial 14 bold")
+        self.to_convert_entry.grid(row=2)
 
         # Conversion button frame (row 3)
 
