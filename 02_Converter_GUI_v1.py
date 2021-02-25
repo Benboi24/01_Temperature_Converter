b28cadad3b7e6d0e2f5f5f8f2b176e3875c82b88
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-02-25 14:54:04 +1300

    partially working converter

--- 02_Converter_GUI_v1.py
+++ 02_Converter_GUI_v1.py
@@ -81,26 +81,37 @@ class Converter:
 
         try:
             to_convert = float(to_convert)
+            print("as a number", to_convert)
             has_errors = "no"
 
-            # Check and convert to Farhrenheit
+            # Check and convert to Fahrenheit
             if low == -273 and to_convert >= low:
                 fahrenheit = (to_convert * 9/5) + 32
                 to_convert = self.round_it(to_convert)
                 fahrenheit = self.round_it(fahrenheit)
                 answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
+                print(answer)
 
             # Convert and convert to Centigrade
             elif low == -459 and to_convert >= low:
                 celsius = (to_convert - 32) * 5/9
                 to_convert = self.round_it(celsius)
-                
+                celsius = self.round_it(celsius)
                 answer = "{} degrees C is {} degrees F".format(to_convert, celsius)
+                print(answer)
 
-            # Round!!
+            else:
+                # Input is invalid (too cold)!!
+                answer = "Too Cold!"
+                has_errors = "yes"
 
             # Display answer
-
+            if has_errors == "no":
+                self.converted_label.configure(text=answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error)
             # Add Answer to list for History
 
         except ValueError:
@@ -108,6 +119,15 @@ class Converter:
             self.to_convert_entry.configure(bg=error)
 
         print("you pushed a button")
+
+    def round_it(self, to_round):
+
+        if to_round % 1 == 0:
+            rounded = "{:.0f".format(to_round)
+        else:
+            rounded = "{:.1f".format(to_round)
+
+        return rounded
 
 # main routine
 if __name__ == "__main__":
